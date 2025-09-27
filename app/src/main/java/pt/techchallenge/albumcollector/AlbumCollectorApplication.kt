273b62db4c7cb7aee64939e2ca213756b9399284
João Guerra <jouis3003@gmail.com>
2025-09-27 15:25:06 +0100

    hilt initial setup

---
+++ app/src/main/java/pt/techchallenge/albumcollector/AlbumCollectorApplication.kt
@@ -0,0 +1,7 @@
+package pt.techchallenge.albumcollector
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class AlbumCollectorApplication: Application()
--- app/src/main/java/pt/techchallenge/albumcollector/ui/MainActivity.kt
+++ app/src/main/java/pt/techchallenge/albumcollector/ui/MainActivity.kt
@@ -4,9 +4,11 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import dagger.hilt.android.AndroidEntryPoint
 import pt.techchallenge.albumcollector.ui.navigation.Navigation
 import pt.techchallenge.albumcollector.ui.theme.AlbumCollectorTheme
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
